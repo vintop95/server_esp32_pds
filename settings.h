3f838ac7518ddf1bc68969407dc52df48e9f2561
hyperfuzz <brivido1991@gmail.com>
2018-11-10 17:01:35 +0100

    Revert "Revert "Making the 1st graphic functioning""
    This reverts commit 023466fbfbb1d5eced9f53267e6c4795649b0b5f.

--- settings.h
+++ settings.h
@@ -11,6 +11,7 @@
 #include <QObject>
 #include <QSettings>
 #include <QSharedPointer>
+#include <QDateTime>
 #include "record.h"
 
 static QString SETTINGS_PATH = "settings.ini";
@@ -18,7 +19,8 @@ static QString SETTINGS_PATH = "settings.ini";
 static quint16 SERVER_PORT = 7856;
 static int ESP32_NO = 4;
 // The application shows the devices in area each CHART_PERIOD ms
-static int CHART_PERIOD = 10000;
+static int CHART_PERIOD = 5*1000; //TODO: 5*60*1000;
+static QDateTime START_TIME = QDateTime::currentDateTime();
 static int ESP32_NO_LIMIT = 15;
 
 /**
